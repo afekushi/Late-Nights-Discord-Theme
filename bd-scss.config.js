baeba34506fe662a0a5c2b42b3b4ce247df1bb67
Aphexi <90076856+afekushi@users.noreply.github.com>
2024-08-25 17:14:16 -0400

    Add files via upload

---
+++ bd-scss.config.js
@@ -0,0 +1,16 @@
+/** @type {import('bd-scss/lib/config').Config} */
+export default {
+	meta: {
+		name: 'Nightcore',
+		author: 'Aphexi',
+		version: '1.0.0',
+		description: 'How discord should be',
+		invite: 'bPU929gDUr',
+		authorId: '868751390758010892',
+		source: 'https://github.com/afekushi/Late-Nights-Discord-Theme'
+	},
+	base: {
+		output: 'dist/dist'
+	},
+	baseImport: 'https://raw.githubusercontent.com/afekushi/Nightcore-Discord-Theme/main/dist/Nightcore.css'
+};
